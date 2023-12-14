de3bb001db47f52b7e55220cf9d9730bb5a53bf7
Harry <hayerdevops@gmail.com>
2023-12-14 04:52:35 -0700

    random direction change instantiation and collision detection similar to player objects

--- game/ghost.py
+++ game/ghost.py
@@ -10,6 +10,23 @@ class Ghost:
         self.speed = 2
         self.direction = pygame.Vector2(random.choice([-1, 1]), 0)
 
+    def update(self):
+        #initiate direction change randomly
+        #and check collisions
+        if random.choice([True, False]):
+            self.change_direction()
+
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y * self.speed
+
+        for y, row in enumerate(self.maze.layout):
+            for x, cell in enumerate(row):
+                if cell == 'X':
+                    wall_rect = pygame.Rect(x * 32, y * 32, 32, 32)
+                    if self.rect.colliderect(wall_rect):
+                        self.change_direction()
+                        break
+
     def change_direction(self):
         #stoichastic direction change
         if random.choice([True, False]):
