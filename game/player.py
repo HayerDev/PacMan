f1b89d7dd79cd0c1d2a853afd130a366830540c0
Harry <hayerdevops@gmail.com>
2023-12-14 03:50:23 -0700

    add update player which invokes key handler method for WASD movements

--- game/player.py
+++ game/player.py
@@ -1,3 +1,4 @@
+import pygame
 class Player:
     def __init__(self):
         # Initialize player properties
@@ -6,4 +7,17 @@ class Player:
         self.speed = 3
         self.direction = pygame.Vector2(0, 0)
 
+    def update(self, maze):
+        self.handle_keys()
+
+    def handle_keys(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.direction = pygame.Vector2(-1, 0)
+        elif keys[pygame.K_d]:
+            self.direction = pygame.Vector2(1, 0)
+        elif keys[pygame.K_w]:
+            self.direction = pygame.Vector2(0, -1)
+        elif keys[pygame.K_s]:
+            self.direction = pygame.Vector2(0, 1)
 
