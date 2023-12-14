6ccacf21433553ad2683a67d0cd1c14c4d35f641
Harry <hayerdevops@gmail.com>
2023-12-14 04:00:02 -0700

    add collision check for player and draw player rect

--- game/player.py
+++ game/player.py
@@ -9,6 +9,7 @@ class Player:
 
     def update(self, maze):
         self.handle_keys()
+        self.move(maze)
 
     def handle_keys(self):
         keys = pygame.key.get_pressed()
@@ -21,3 +22,31 @@ class Player:
         elif keys[pygame.K_s]:
             self.direction = pygame.Vector2(0, 1)
 
+    def move(self, maze):
+        #lateral
+        self.rect.x += self.direction.x * self.speed
+        self.check_collision(maze, self.direction.x, 0)
+
+        #vert
+        self.rect.y += self.direction.y * self.speed
+        self.check_collision(maze, 0, self.direction.y)
+
+    def check_collision(self, maze, dx, dy):
+        #look at each cell
+        for y, row in enumerate(maze.layout):
+            for x, cell in enumerate(row):
+                if cell == 1:
+                    wall_rect = pygame.Rect(x * 32, y * 32, 32, 32)
+                    #movements wall hits
+                    if self.rect.colliderect(wall_rect):
+                        if dx > 0:
+                            self.rect.right = wall_rect.left
+                        if dx < 0:
+                            self.rect.left = wall_rect.right
+                        if dy > 0:
+                            self.rect.bottom = wall_rect.top
+                        if dy < 0:
+                            self.rect.top = wall_rect.bottom
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
